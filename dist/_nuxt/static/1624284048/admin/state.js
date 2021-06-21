4229dcce1798b8a9722494f37523a436ef0682cc
codewithali <ali-raza@ashlarglobal.com>
2021-06-21 19:01:07 +0500

    route update

---
+++ dist/_nuxt/static/1624284048/admin/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-test\u002F_nuxt\u002Fstatic\u002F1624284048",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin",config:{_app:{basePath:"\u002Fnuxt-test\u002F",assetsPath:"\u002Fnuxt-test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
